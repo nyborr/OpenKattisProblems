3e909af7c2dc1ae1728c3148bbb01347430b6d95
DESKTOP-2P4C3GV\robyn <robynlynnyates@hotmail.com>
2018-09-03 15:15:55 -0500

    UpdateToAboveAvgProblem
    Still getting error for second part.

--- AboveAverage.java
+++ AboveAverage.java
@@ -16,14 +16,15 @@ public class AboveAverage {
 		DecimalFormat df = new DecimalFormat("#.000");
 		
 		for(int i = 0; i < C; i++) {
-			String[] str = classes[i].split(" ");	
+			String[] str = classes[i].split(" ");
 			for (int j = 1; j < Integer.valueOf(str[0]) + 1; j++) {
 				sum += Double.valueOf(str[j]);
 			}
 			avgs[i] = sum / Double.valueOf(str[0]);
-			//System.out.println(avgs[i]);
+			//System.out.println(avgs[i]); 
 			sum = 0;
 		}
+		
 		
 		int[] aboveAvg = new int[C];
 		for(int i = 0; i < C; i++) {
@@ -39,7 +40,7 @@ public class AboveAverage {
 		}
 		
 		for(int i = 0; i < C; i++) {
-			System.out.println(aboveAvg[i]);
+			//System.out.println(aboveAvg[i]);
 			String[] str = classes[i].split(" ");
 			System.out.println(df.format((aboveAvg[i]/Double.valueOf(str[0])*100))+"%");
 		}
